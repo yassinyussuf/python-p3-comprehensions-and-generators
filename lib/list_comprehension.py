d5139cbab7a2adb799b62acb529a744f5cb85d2b
yassinyussuf <yassinyussuf414@gmail.com>
2024-05-28 09:37:54 +0300

    complete lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    evens = [num for num in num_list if num % 2 == 0]
+    return evens
 
+    
 def make_exclamation(sentence_list):
-    pass
+    if not sentence_list:  # checks if the list is empty
+        return []
+    else:
+        return [sentence + '!' for sentence in sentence_list]
